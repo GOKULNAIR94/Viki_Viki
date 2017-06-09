d005c687c3c1bcfcfbe1f6eb8da55bf05a5c25d7
Gokul Nair <gokulgnair94@gmail.com>
2017-06-09 11:13:03 +0530

    iBot

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ restService.post('/inputmsg', function(req, res) {
   var query = '';
 
   //req =  JSON.parse(content);
-    console.log( "Req : " + JSON.stringify(req.body) );
+    console.log( "Req : " + JSON.stringify(req) );
 
     var intentName = req.body.result.metadata.intentName;
     Market = req.body.result.parameters.Market;
