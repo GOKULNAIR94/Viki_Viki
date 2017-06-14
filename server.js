f7d9a451c13e73d405b823211729225395ebad19
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-06-14 18:56:18 +0530

    followup

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ restService.post('/inputmsg', function(req, res) {
           return res.json({
               speech: speech,
               displayText: speech,
-              contextOut: [{ "name" : "Budget-followup", "lifespan":1, "parameters":{ track : "Baywatch" }}]
+              contextOut: [{ "name" : "budget-followup", "lifespan":1, "parameters":{ track : "Baywatch" }}]
               //source: 'webhook-OSC-oppty'
           })
       }
