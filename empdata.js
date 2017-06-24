4204dc2faa4534a2524f6dfdfc9824750b7fbac1
Gokul Nair <gokulgnair94@gmail.com>
2017-06-24 12:40:15 +0530

    Emp Data

--- empdata.js
+++ empdata.js
@@ -1,6 +1,7 @@
 module.exports = function(req, res) {
 
   var fs = require('fs');
+  var jsonQuery = require('json-query');
 
   var content;
 
@@ -9,4 +10,20 @@ module.exports = function(req, res) {
   content = JSON.parse(content);
 
   console.log("Content :" + JSON.stringify(content));
+  
+  var Name = req.body.result.contexts[0].parameters['Name.original'];
+  var attrib = req.body.result.parameters['DCP_AttribsGeneral'];
+  
+  var query = "Name=" + Name ;
+  var output =
+          jsonQuery('[* '+ query +']'+'['+ attrib +']', {
+            data: content
+          }).value;
+  console.log("output :" + output);
+  return res.json({
+    speech: output,
+    displayText: output,
+    //source: 'webhook-OSC-oppty'
+  })
+
 }
