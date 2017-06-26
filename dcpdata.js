fd074e531b28e6eb23587c071dc6fd6aad8ee2d0
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-06-26 12:05:55 +0530

    Term

--- dcpdata.js
+++ dcpdata.js
@@ -9,22 +9,51 @@ module.exports = function(req, res) {
 
   var Name = "";
   var attrib = "";
+    var dateperiodOG = "";
+    var attribOG = "";
+    var countFlag = 0;
 
   var filePath = "";
   var query = "";
 
-  if( intentName == "DCP - EmployeeData" ) 
+    attrib = req.body.result.parameters['DCP_AttribsGeneral'];
+  attribOG = req.body.result.contexts[0].parameters['DCP_AttribsGeneral.original'];
+    if( intentName == "DCP - EmployeeData" ) 
   {
-    Name = req.body.result.contexts[0].parameters['Name.original']
-    attrib = req.body.result.parameters['DCP_AttribsGeneral']
+    Name = req.body.result.contexts[0].parameters['Name.original'];
     filePath = "./data/EmployeeData.json";
     query = "Name=" + Name ;
   }
 
   if( intentName == "DCP - HireTerm" ) 
   {
-    filePath = "./data/EmployeeData.json";
-    //if(  )
+    
+    if( attrib != null){
+        Name = req.body.result.contexts[0].parameters['Name.original'];
+        query = "Name=" + Name ;
+    }
+    else{
+        var dateperiod = req.body.result.parameters.dateperiod;
+        dateperiodOG = req.body.result.contexts[0].parameters['date-period.original'];
+        var StartDate = dateperiod.split("/")[0];
+        var EndDate = dateperiod.split("/")[1];
+        
+        if( req.body.result.parameters['HireTerm'] == "Hire"){
+            query = "Hire Date>=" + StartDate "& Hire Date<=" + EndDate;
+        }
+
+        if( req.body.result.parameters['HireTerm'] == "Term"){
+            query = "Termination Date>=" + StartDate "& Termination Date<=" + EndDate;
+        }
+    }
+      
+    if( req.body.result.parameters['HireTerm'] == "Hire"){
+        filePath = "./data/Hire.json";    
+    }
+      
+    if( req.body.result.parameters['HireTerm'] == "Term"){
+        filePath = "./data/Termination .json";
+    }
   }
 
   content = fs.readFileSync( filePath, 'utf8' );
@@ -50,6 +79,20 @@ module.exports = function(req, res) {
     if( output.length == 1 ){
       speech = output[0];
     }
+      else
+          if(output.length > 1){
+              if( countFlag == 1 ){
+                  var sum = 0;
+                    for(var i =0; i < output.length; i++)
+                    {
+                      sum = sum + parseFloat(output[i]);
+                    } 
+                  speech = sum + " " + attribOG + " " + dateperiodOG + ".";
+              }
+              else{
+                  speech = "More than one record found.";
+              }
+          }
   }
    
   
