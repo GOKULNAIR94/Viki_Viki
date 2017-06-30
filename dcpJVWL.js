eb1c9010e6e3668c7e833fea42f419f188387188
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-06-30 17:51:01 +0530

    Test

--- dcpJVWL.js
+++ dcpJVWL.js
@@ -1,3 +1,45 @@
 module.exports = function(req, res) {
+    var intentName = req.body.result.metadata.intentName;
+    var fs = require('fs');
+    var jsonQuery = require('json-query');
 
+    var content;
+    var speech;
+    
+    var jrnlId = req.body.result.contexts[0].parameters['JOURNAL_ID'];
+    var attrib = req.body.result.contexts[0].parameters['JRNLAttrib'];
+    
+    var filePath = "";
+    var query = "";
+    
+    query = "JOURNAL_ID = " + jrnlId;
+    
+    content = fs.readFileSync(filePath, 'utf8');
+
+    content = JSON.parse(content);
+    console.log("Content :" + JSON.stringify(content));
+
+    var output =
+        jsonQuery('[* ' + query + ']' + '[' + attrib + ']', {
+            data: content
+        }).value;
+    
+    if (output.length == 0) {
+        speech = "No records found.";
+    } else {
+        if (output.length == 1) {
+            if ( intentName.indexOf( "DCP - JRNL" ) == 0 ){
+                speech = "The " + attrib + " of journel" + jrnlId + " is " + output + ".";
+            }
+        } else
+            if (output.length > 1) {
+                speech = "More than one record found.";
+            }
+    }
+    
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        //source: 'webhook-OSC-oppty'
+    })
 }
