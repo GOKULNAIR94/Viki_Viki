ed081cc74fea04d2e0fd995f24853f029a0cc896
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-07-07 15:46:03 +0530

    Query

--- dcpJVWL.js
+++ dcpJVWL.js
@@ -35,11 +35,11 @@ module.exports = function(req, res) {
             console.log("output : " + output);
             if ( output  == "Posted" ){
                 var posteddate = content[i]["POSTED_DATE"];
-                speech = "The status of journal " + jrnlId + " is " + output + ".\n Posted date is : " + posteddate;
+                speech = "The status of journal " + jrnlId + " is " + output + ".\n Posted date is : " + posteddate + "!";
             }
             if( output == "Error"){
                 var errormsg = content[i]["ERROR"];
-                speech = "The status of journal " + jrnlId + " is " + output + ".\n Reason is : " + errormsg;
+                speech = "The status of journal " + jrnlId + " is " + output + ".\n Reason is : " + errormsg + "!";
             }
             if( output == "New"){
                 speech = "The status of journal " + jrnlId + " is " + output + ".";
