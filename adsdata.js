4e3d7a28570b4b3b6b50feb9b90e2538d87a8350
Gokul Nair <gokulgnair94@gmail.com>
2017-09-14 20:52:54 +0530

    RN

--- adsdata.js
+++ adsdata.js
@@ -17,6 +17,68 @@ module.exports = function(req, resp) {
     var query = "";
     var HeadCountQuery = "";
     var os = require('os');
+    var qPriotity;
+    
+    if ( intentName == "ADS_SNIncidents_list") {
+        
+        qPriotity = req.body.result.parameters['ADS_RN_Priority'];
+        var qPath ="";
+        if( qPriotity == "open"){
+            qPath = "services/rest/connect/latest/incidents?q=statusWithType.status.lookupName='Unresolved'";
+        }
+        else{
+            qPath = "services/rest/connect/latest/incidents?q=customFields.c.priority.lookupName='" + qPriotity + "'";
+        }
+        
+        var options = {
+            host: 'ntinfotech--tst.custhelp.com',
+            path: qPath,
+            headers: {
+                'Authorization': 'Basic cHBhdGthcjpsbnRMTlQxMjM0'
+            }
+        };
+        console.log("Here intentName : " + intentName);
+        var output;
+        var r = https.get(options, function(res) {
+            var body = "";
+
+            res.on('data', function(data) {
+                body += data;
+            });
+            res.on('end', function() {
+                console.log("Body : " + body);
+                output = JSON.parse(body);
+                
+                
+                //var incDesc = output.customFields.c.description;
+                
+                
+                
+                
+//                if ( incSubject != null && incSubject != "")
+//                    speech = speech + "Subject : " + incSubject + "." + os.EOL;
+//
+//                if ( incStatus != null && incStatus != "")
+//                    speech = speech + "Status : " + incStatus + "." + os.EOL;
+//
+//                if ( incPrior != null && incPrior != "")
+//                    speech = speech + "Priority : " + incPrior + "." + os.EOL;
+//
+//                if ( incDesc != null && incDesc != "")
+//                    speech = speech + "Notes : " + incDesc + "." + os.EOL;
+//
+//                return resp.json({
+//                    speech: speech,
+//                    displayText: speech,
+//                    //source: 'webhook-OSC-oppty'
+//                });
+
+            })
+        }).on('error', function(e) {
+            console.error(e);
+        });
+        
+    }
 
 
     if (intentName == "ADS_SNIncident" || intentName == "ADS_SNIncidents_list - custom") {
