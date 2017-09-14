f775c4dc50b847a64fe5d85ed12e7121f9203706
Gokul Nair <gokulgnair94@gmail.com>
2017-09-14 11:29:32 +0530

    Master Viki

--- adsdata.js
+++ adsdata.js
@@ -2,7 +2,7 @@ module.exports = function(req, res) {
 
     var fs = require('fs');
     var jsonQuery = require('json-query');
-    
+
     var content;
     var speech;
     var intentName = req.body.result.metadata.intentName;
@@ -17,90 +17,103 @@ module.exports = function(req, res) {
     var query = "";
     var HeadCountQuery = "";
     var os = require('os');
-    
 
-    if ( intentName == "ADS_SNIncident" || intentName == "ADS_SNIncidents_list - custom"  ) {
-        //attrib = req.body.result.parameters['ADS_attrib'];
-        
-        number = req.body.result.parameters['Number'];
-        
-        filePath = "./data/ServiceNow.json";
-        
-        query = [{
-            "key" : "Number",
-            "opt" : "==",
-            "value" : number
-        }];
+    var SendEmail = require("./sendEmail");
+    var sendAdhocEmail = require("./sendAdhocEmail");
+    
+    if (intentName == 'ADS_HyperionReport') {
+        SendEmail(req, res, function(result) {
+            console.log("SendEmail Called");
+        });
     }
-    if ( intentName == "ADS_SNIncidents_list" ) {
-        filePath = "./data/ServiceNow.json";
-        query = [{
-            "key" : "Active",
-            "opt" : "==",
-            "value" : true
-        }];
+    if (intentName == 'ADS_AdhocData') {
+        sendAdhocEmail(req, res, function(result) {
+            console.log("SendEmail Called");
+        });
     }
-    
-    content = fs.readFileSync(filePath, 'utf8');
-    content = JSON.parse(content);
-    
-    //console.log("Content :" + JSON.stringify(content));
-    console.log("query :" + query);
-    console.log("attrib :" + attrib);
-    
-    var output = [];
-    for(var i = 0; i < content.length; i++){
 
-        if ( intentName != "ADS_SNIncidents_list" ) {
-            theString = '"' + content[i][query[0].key].toString().toLowerCase() + '"' + query[0].opt + '"' +  query[0].value.toString().toLowerCase() + '"';
+
+    if (intentName != 'ADS_HyperionReport' && intentName != 'ADS_AdhocData') {
+        if (intentName == "ADS_SNIncident" || intentName == "ADS_SNIncidents_list - custom") {
+            //attrib = req.body.result.parameters['ADS_attrib'];
+
+            number = req.body.result.parameters['Number'];
+
+            filePath = "./data/ServiceNow.json";
+
+            query = [{
+                "key": "Number",
+                "opt": "==",
+                "value": number
+            }];
         }
-        else{
-            theString = '"' + content[i][query[0].key].toString().toLowerCase() + '"' + query[0].opt + '"' +  query[0].value.toString().toLowerCase() + '"';
-            theString = theString + ' && ' + '"' + content[i]["Assigned to"] + '" == "David Loo"';
+        if (intentName == "ADS_SNIncidents_list") {
+            filePath = "./data/ServiceNow.json";
+            query = [{
+                "key": "Active",
+                "opt": "==",
+                "value": true
+            }];
         }
-        console.log( "The String : " + theString );
-        if( eval( theString ) ){
-            output.push( content[i] );
-            //console.log("output :" + output);
-        }
-    }
-    
-    //console.log("output :" + output);
-    if (output.length == 0) {
-        speech = "No records found.";
-    }
-    else{
-        if( output.length == 1 ){
-            speech = "Details of incident " + number + ": " + os.EOL;
-            
-            if( output[0]["Opened"] != null && output[0]["Opened"] != "" )
-                speech = speech + "Opened date : " + output[0]["Opened"] + "." + os.EOL; 
-            
-            if( output[0]["State"] != null && output[0]["State"] != "" )
-                speech = speech + "Status : " + output[0]["State"] + "." + os.EOL; 
-            
-            if( output[0]["Short description"] != null && output[0]["Short description"] != "" )
-                speech = speech + "Description : " + output[0]["Short description"] + "." + os.EOL; 
-            
-            if( output[0]["Comments and Work notes"] != null && output[0]["Comments and Work notes"] != "" )
-                speech = speech + "Notes : " + output[0]["Comments and Work notes"] + "." + os.EOL;
-                
-        }
-        else{
-            if ( intentName == "ADS_SNIncidents_list" ) {
-                speech = "List of incidents : "+ os.EOL;
-                for(var i = 0; i < output.length; i++){
-                    speech = speech + " " + (i+1) + " - " + output[i]["Number"] + "." + os.EOL;
-                }
+
+        content = fs.readFileSync(filePath, 'utf8');
+        content = JSON.parse(content);
+
+        //console.log("Content :" + JSON.stringify(content));
+        console.log("query :" + query);
+        console.log("attrib :" + attrib);
+
+        var output = [];
+        for (var i = 0; i < content.length; i++) {
+
+            if (intentName != "ADS_SNIncidents_list") {
+                theString = '"' + content[i][query[0].key].toString().toLowerCase() + '"' + query[0].opt + '"' + query[0].value.toString().toLowerCase() + '"';
+            } else {
+                theString = '"' + content[i][query[0].key].toString().toLowerCase() + '"' + query[0].opt + '"' + query[0].value.toString().toLowerCase() + '"';
+                theString = theString + ' && ' + '"' + content[i]["Assigned to"] + '" == "David Loo"';
             }
-            
+            console.log("The String : " + theString);
+            if (eval(theString)) {
+                output.push(content[i]);
+                //console.log("output :" + output);
+            }
         }
+
+        //console.log("output :" + output);
+        if (output.length == 0) {
+            speech = "No records found.";
+        } else {
+            if (output.length == 1) {
+                speech = "Details of incident " + number + ": " + os.EOL;
+
+                if (output[0]["Opened"] != null && output[0]["Opened"] != "")
+                    speech = speech + "Opened date : " + output[0]["Opened"] + "." + os.EOL;
+
+                if (output[0]["State"] != null && output[0]["State"] != "")
+                    speech = speech + "Status : " + output[0]["State"] + "." + os.EOL;
+
+                if (output[0]["Short description"] != null && output[0]["Short description"] != "")
+                    speech = speech + "Description : " + output[0]["Short description"] + "." + os.EOL;
+
+                if (output[0]["Comments and Work notes"] != null && output[0]["Comments and Work notes"] != "")
+                    speech = speech + "Notes : " + output[0]["Comments and Work notes"] + "." + os.EOL;
+
+            } else {
+                if (intentName == "ADS_SNIncidents_list") {
+                    speech = "List of incidents : " + os.EOL;
+                    for (var i = 0; i < output.length; i++) {
+                        speech = speech + " " + (i + 1) + " - " + output[i]["Number"] + "." + os.EOL;
+                    }
+                }
+
+            }
+        }
+
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            //source: 'webhook-OSC-oppty'
+        });
     }
-    
-    return res.json({
-        speech: speech,
-        displayText: speech,
-        //source: 'webhook-OSC-oppty'
-    });
 
 }
--- server.js
+++ server.js
@@ -15,174 +15,156 @@ var jsonQuery = require('json-query');
 
 var DCPData = require("./dcpdata");
 var ADSData = require("./adsdata");
-var SendEmail = require("./sendEmail");
-var sendAdhocEmail = require("./sendAdhocEmail");
 
 restService.post('/inputmsg', function(req, res) {
 
-  var Market = '';
-  var Period = '';
-  var dateperiod = '';
-  var Month = '';
+    var Market = '';
+    var Period = '';
+    var dateperiod = '';
+    var Month = '';
 
-  var speech = '';
-  var query = '';
+    var speech = '';
+    var query = '';
 
-  //req =  JSON.parse(content);
+    //req =  JSON.parse(content);
     //console.log( "Req : " + JSON.stringify(req.body) );
 
     var intentName = req.body.result.metadata.intentName;
-    
-console.log( "intentName THIS : " + intentName );
+
+    console.log("intentName THIS : " + intentName);
     var content;
-    try
-    {
-      if(intentName == 'Budget' ){
-        Market = req.body.result.parameters.Market;
-        Period = req.body.result.parameters.Period;
-        dateperiod = req.body.result.parameters.dateperiod;
-        Month = dateperiod.split("/")[1];
+    try {
+        if (intentName == 'Budget') {
+            Market = req.body.result.parameters.Market;
+            Period = req.body.result.parameters.Period;
+            dateperiod = req.body.result.parameters.dateperiod;
+            Month = dateperiod.split("/")[1];
 
-        
-        console.log( "Market : " + Market );
-        console.log( "Period : " + Period );
-        console.log( "dateperiod : " + dateperiod );
-        console.log( "Month : " + Month );
 
-        content = fs.readFileSync('data.json', 'utf8');
-        console.log( "Content : " + content);
-        content = JSON.parse(content);
-        
-        if( Market != "Global" ){
-          query = "Market=" + Market + " & ";
-        }
-        if( Period == "MTD" ){
-          query = query + "Month=" + Month;
-          //query = query + "Period=" + Period + " & Month=" + Month;
-        }
-        else{
-          var fyStartDate ="";
-          var arr = Month.split("-");
-          if( arr[1] >= 4 ){
-            fyStartDate = arr[0] + "-0" + 4 + "-" + 1;
-          }
-          else{
-            fyStartDate = (parseInt(arr[0]) - 1) + "-0" + 4 + "-" + 1;
-          }
-          console.log( "fyStartDate : " + fyStartDate);
-          query = query + "Month >= " + fyStartDate + " & Month <= " + Month;
-        }
-        
+            console.log("Market : " + Market);
+            console.log("Period : " + Period);
+            console.log("dateperiod : " + dateperiod);
+            console.log("Month : " + Month);
 
-        console.log( "query : " + query );
-        var output =
-          jsonQuery('items[* '+ query +'][Remaining Budget]', {
-            data: content
-          }).value;
-          console.log( "Output : " + output);
-          console.log( "Output Length : " + output.length);
-          if( output.length == 0 ){
-            speech = "No records found."
-          }
-            if( output.length == 1 ){
-            console.log( "Output : " + output);
-            speech = "The remaining YTD Budget for Entertainment is " + output + "$. Is there anything else I can help you with?"
-          }
-          if( output.length > 1 )
-          {
-            var sum = 0;
-            for(var i =0; i < output.length; i++)
-            {
-              sum = sum + parseFloat(output[i]);
+            content = fs.readFileSync('data.json', 'utf8');
+            console.log("Content : " + content);
+            content = JSON.parse(content);
+
+            if (Market != "Global") {
+                query = "Market=" + Market + " & ";
             }
-            console.log( "Sum : " + sum);
-            speech = "The remaining YTD Budget for Entertainment is " + sum + "$. Is there anything else I can help you with?"
-          }
-          return res.json({
-              speech: speech,
-              displayText: speech,
-              //source: 'webhook-OSC-oppty'
-          })
-      }
+            if (Period == "MTD") {
+                query = query + "Month=" + Month;
+                //query = query + "Period=" + Period + " & Month=" + Month;
+            } else {
+                var fyStartDate = "";
+                var arr = Month.split("-");
+                if (arr[1] >= 4) {
+                    fyStartDate = arr[0] + "-0" + 4 + "-" + 1;
+                } else {
+                    fyStartDate = (parseInt(arr[0]) - 1) + "-0" + 4 + "-" + 1;
+                }
+                console.log("fyStartDate : " + fyStartDate);
+                query = query + "Month >= " + fyStartDate + " & Month <= " + Month;
+            }
 
-      if(intentName == 'Expense' ){
-        Market = req.body.result.parameters.Market;
-        Period = req.body.result.parameters.Period;
-        dateperiod = req.body.result.parameters.dateperiod;
-        Month = dateperiod.split("/")[1];
 
-        console.log( "intentName : " + intentName );
-        console.log( "Market : " + Market );
-        console.log( "Period : " + Period );
-        console.log( "dateperiod : " + dateperiod );
-        console.log( "Month : " + Month );
-        
-        
-        content = fs.readFileSync('data.json', 'utf8');
-        console.log( "Content : " + content);
-        content = JSON.parse(content);
-        
-        query = "Month = " + Month;
-
-        if( Market != "Global" ){
-          query = query + " & Market =" + Market;
+            console.log("query : " + query);
+            var output =
+                jsonQuery('items[* ' + query + '][Remaining Budget]', {
+                    data: content
+                }).value;
+            console.log("Output : " + output);
+            console.log("Output Length : " + output.length);
+            if (output.length == 0) {
+                speech = "No records found."
+            }
+            if (output.length == 1) {
+                console.log("Output : " + output);
+                speech = "The remaining YTD Budget for Entertainment is " + output + "$. Is there anything else I can help you with?"
+            }
+            if (output.length > 1) {
+                var sum = 0;
+                for (var i = 0; i < output.length; i++) {
+                    sum = sum + parseFloat(output[i]);
+                }
+                console.log("Sum : " + sum);
+                speech = "The remaining YTD Budget for Entertainment is " + sum + "$. Is there anything else I can help you with?"
+            }
+            return res.json({
+                speech: speech,
+                displayText: speech,
+                //source: 'webhook-OSC-oppty'
+            })
         }
 
-        console.log( "query : " + query );
-        var output =
-          jsonQuery('items[* '+ query +'][Expense]', {
-            data: content
-          }).value;
-          console.log( "Output : " + output);
-          console.log( "Output Length : " + output.length);
-          if( output.length == 0 ){
-            speech = "No records found."
-          }
-            if( output.length == 1 ){
-            console.log( "Output : " + output);
-            speech = "The Entertainment expense for " + Market + " is " + output + "$. Is there anything else I can help you with?"
-          }
-          if( output.length > 1 )
-          {
-            var sum = 0;
-            for(var i =0; i < output.length; i++)
-            {
-              sum = sum + parseFloat(output[i]);
+        if (intentName == 'Expense') {
+            Market = req.body.result.parameters.Market;
+            Period = req.body.result.parameters.Period;
+            dateperiod = req.body.result.parameters.dateperiod;
+            Month = dateperiod.split("/")[1];
+
+            console.log("intentName : " + intentName);
+            console.log("Market : " + Market);
+            console.log("Period : " + Period);
+            console.log("dateperiod : " + dateperiod);
+            console.log("Month : " + Month);
+
+
+            content = fs.readFileSync('data.json', 'utf8');
+            console.log("Content : " + content);
+            content = JSON.parse(content);
+
+            query = "Month = " + Month;
+
+            if (Market != "Global") {
+                query = query + " & Market =" + Market;
             }
-            console.log( "Sum : " + sum);
-            speech = "The Entertainment expense for " + Market + " is " + sum + "$. Is there anything else I can help you with?"
-          }
-          return res.json({
-              speech: speech,
-              displayText: speech,
-              //source: 'webhook-OSC-oppty'
-          })
-      }
-      if( intentName.indexOf( "DCP -" ) == 0 ){
-        DCPData(req, res, function(result) {
-          console.log("EmpData Called");
-        });
-      }
-      if( intentName.indexOf( "ADS_" ) == 0 && intentName != 'ADS_HyperionReport' && intentName != 'ADS_AdhocData' ){
-        ADSData(req, res, function(result) {
-          console.log("ADSData Called");
-        });
-      }
-      if(intentName == 'ADS_HyperionReport' ){
-        SendEmail(req, res, function(result) {
-          console.log("SendEmail Called");
-        });
-      }
-    if(intentName == 'ADS_AdhocData' ){
-        sendAdhocEmail(req, res, function(result) {
-          console.log("SendEmail Called");
-        });
-      }
+
+            console.log("query : " + query);
+            var output =
+                jsonQuery('items[* ' + query + '][Expense]', {
+                    data: content
+                }).value;
+            console.log("Output : " + output);
+            console.log("Output Length : " + output.length);
+            if (output.length == 0) {
+                speech = "No records found."
+            }
+            if (output.length == 1) {
+                console.log("Output : " + output);
+                speech = "The Entertainment expense for " + Market + " is " + output + "$. Is there anything else I can help you with?"
+            }
+            if (output.length > 1) {
+                var sum = 0;
+                for (var i = 0; i < output.length; i++) {
+                    sum = sum + parseFloat(output[i]);
+                }
+                console.log("Sum : " + sum);
+                speech = "The Entertainment expense for " + Market + " is " + sum + "$. Is there anything else I can help you with?"
+            }
+            return res.json({
+                speech: speech,
+                displayText: speech,
+                //source: 'webhook-OSC-oppty'
+            })
+        }
+        if (intentName.indexOf("DCP -") == 0) {
+            DCPData(req, res, function(result) {
+                console.log("EmpData Called");
+            });
+        }
+
+        if (intentName.indexOf("ADS_") == 0 ) {
+            ADSData(req, res, function(result) {
+                console.log("ADSData Called");
+            });
+        }
+
         
-    }
-    catch(e)
-    {
-        console.log("Error : " + e );
+
+    } catch (e) {
+        console.log("Error : " + e);
     }
 
 });
