47effdf199b8ff1a74bc90174a5643a35185af7d
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-09-15 11:50:54 +0530

    Create

--- createTicket.js
+++ createTicket.js
@@ -4,32 +4,20 @@ module.exports = function(ticket, response) {
     var speech = "";
     console.log( "ticket : " + JSON.stringify(ticket));
     
-    var options = {
-      "method": "POST",
-      "hostname": "ntinfotech--tst.custhelp.com",
-      "port": null,
-      "path": "/services/rest/connect/latest/incidents/",
-      "headers": {
-        "authorization": "Basic cHBhdGthcjpsbnRMTlQxMjM0",
-        "cache-control": "no-cache",
-        "postman-token": "0f6c028e-bdc2-ecff-3b65-da4e82a4acd5"
-      }
-    };
-    
-    
-    var req = http.request(options, function (res) {
-      var chunks = [];
+    var request = require("request");
 
-      res.on("data", function (chunk) {
-        chunks.push(chunk);
-      });
+    var options = { method: 'POST',
+      url: 'https://ntinfotech--tst.custhelp.com/services/rest/connect/latest/incidents/',
+      headers: 
+       { 'postman-token': '2e164913-425e-4d15-e09a-5d479d31c1d5',
+         'cache-control': 'no-cache',
+         authorization: 'Basic cHBhdGthcjpsbnRMTlQxMjM0' },
+      body: '{\r\n    "primaryContact": {\r\n        "id": 60\r\n    },\r\n    "assignedTo": {\r\n        "staffGroup": {\r\n            "lookupName": "Admin"\r\n        }\r\n    },\r\n    "customFields": {\r\n        "c": {\r\n            "description": "no",\r\n            "priority": {\r\n                "lookupName": "1 - High"\r\n            }\r\n        }\r\n    },\r\n    "statusWithType": {\r\n        "status": {\r\n            "lookupName": "Unresolved"\r\n        }\r\n    },\r\n    "subject": "GL data is missing in FinRpt for 2017 for Actuals scenario 1"\r\n}' };
 
-      res.on("end", function () {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-      });
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+
+      console.log(body);
     });
 
-    req.write("{\r\n    \"primaryContact\": {\r\n        \"id\": 60\r\n    },\r\n    \"assignedTo\": {\r\n        \"staffGroup\": {\r\n            \"lookupName\": \"Admin\"\r\n        }\r\n    },\r\n    \"customFields\": {\r\n        \"c\": {\r\n            \"description\": \"no\",\r\n            \"priority\": {\r\n                \"lookupName\": \"1 - High\"\r\n            }\r\n        }\r\n    },\r\n    \"statusWithType\": {\r\n        \"status\": {\r\n            \"lookupName\": \"Unresolved\"\r\n        }\r\n    },\r\n    \"subject\": \"GL data is missing in FinRpt for 2017 for Actuals scenario 1\"\r\n}");
-    req.end();
 }
