ecd5f1a2c9fb097d097e02fa0d7a43abb8e63059
Gokul Nair <gokulgnair94@gmail.com>
2017-09-25 20:11:41 +0530

    RN TIcket

--- adsdata.js
+++ adsdata.js
@@ -1,23 +1,11 @@
 module.exports = function(req, resp) {
-    var https = require('https');
-    var fs = require('fs');
-    var jsonQuery = require('json-query');
     
     var Query = require("./query");
 
-    var content;
+    
     var speech = "";
     var intentName = req.body.result.metadata.intentName;
 
-    var Name = "";
-    var attrib = "";
-    var HireTermOG = "";
-    var attribOG = "";
-    var countFlag = 0;
-
-    var filePath = "";
-    var query = "";
-    var HeadCountQuery = "";
     var os = require('os');
     var qPriotity;
     var varAuth = 'Basic cHBhdGthcjpsbnRMTlQxMjM0';
@@ -51,60 +39,6 @@ module.exports = function(req, resp) {
                 //source: 'webhook-OSC-oppty'
             });
         });
-//        var options = {
-//            host: 'ntinfotech--tst.custhelp.com',
-//            path: qPath,
-//            headers: {
-//                'Authorization': varAuth
-//            }
-//        };
-//        
-//        var output;
-//        var r = https.get(options, function(resp) {
-//            var body = "";
-//
-//            resp.on('data', function(data) {
-//                body += data;
-//            });
-//            resp.on('end', function() {
-//                console.log("Body : " + body);
-//                output = JSON.parse(body);
-//                
-//                for(var i = 0; i < output.items.length; i++){
-//                    speech = speech + "Incident " + output.items[i].lookupName + ": " + output.items[i].subject + ". " + os.EOL;
-//                }
-//                
-//                
-//                return resp.json({
-//                    speech: speech,
-//                    displayText: speech,
-//                    //source: 'webhook-OSC-oppty'
-//                });
-//                
-//                
-//                
-////                if ( incSubject != null && incSubject != "")
-////                    speech = speech + "Subject : " + incSubject + "." + os.EOL;
-////
-////                if ( incStatus != null && incStatus != "")
-////                    speech = speech + "Status : " + incStatus + "." + os.EOL;
-////
-////                if ( incPrior != null && incPrior != "")
-////                    speech = speech + "Priority : " + incPrior + "." + os.EOL;
-////
-////                if ( incDesc != null && incDesc != "")
-////                    speech = speech + "Notes : " + incDesc + "." + os.EOL;
-////
-////                return resp.json({
-////                    speech: speech,
-////                    displayText: speech,
-////                    //source: 'webhook-OSC-oppty'
-////                });
-//
-//            })
-//        }).on('error', function(e) {
-//            console.error(e);
-//        });
         
     }
 
@@ -153,57 +87,6 @@ module.exports = function(req, resp) {
             }
 
         });
-//        var options = {
-//            host: 'ntinfotech--tst.custhelp.com',
-//            path: '/services/rest/connect/latest/incidents/' + number,
-//            headers: {
-//                'Authorization': varAuth
-//            }
-//        };
-//        console.log("Here intentName : " + intentName);
-//        var output;
-//        var r = https.get(options, function(res) {
-//            var body = "";
-//
-//            res.on('data', function(data) {
-//                body += data;
-//            });
-//            res.on('end', function() {
-//                console.log("Body : " + body);
-//                output = JSON.parse(body);
-//                
-//                
-//                var incDesc = output.customFields.c.description;
-//                var incPrior = output.customFields.c.priority.lookupName;
-//                var incStatus = output.statusWithType.status.lookupName;
-//                var incSubject = output.subject;
-                
-                
-                
-                if ( incSubject != null && incSubject != "")
-                    speech = speech + "Subject : " + incSubject + "." + os.EOL;
-
-                if ( incStatus != null && incStatus != "")
-                    speech = speech + "Status : " + incStatus + "." + os.EOL;
-
-                if ( incPrior != null && incPrior != "")
-                    speech = speech + "Priority : " + incPrior + "." + os.EOL;
-
-                if ( incDesc != null && incDesc != "")
-                    speech = speech + "Notes : " + incDesc + "." + os.EOL;
-
-//                return resp.json({
-//                    speech: speech,
-//                    displayText: speech,
-//                    //source: 'webhook-OSC-oppty'
-//                });
-//
-//            })
-//        }).on('error', function(e) {
-//            console.error(e);
-//        });
-
-        
 
     }
 }
