4b5602c7752bd075ec456fd7e13e41b6e5bb442b
Gokul Nair <gokulgnair94@gmail.com>
2018-02-02 01:25:41 +0530

    KIR

--- osc/partners.js
+++ osc/partners.js
@@ -3,28 +3,43 @@ module.exports = function ( req, res, callback){
     var Query = require("./query");
     var loginEncoded="", qString="";
     var speech="";
-    var suggests = [];
+    var suggests = [{"title": "Show me Opportunities"}];
+    var suggPatners = [];
+    var intentName = req.body.result.metadata.intentName;
     
-
     qString = "/salesApi/resources/latest/partners?onlyData=true";
         
-        Query( qString, loginEncoded, req, res, function(result) {
-            if( result.items.length <= 0 ){
-                speech = "No records found";
-            }
-            else{
-                for(var i = 0; i < result.items.length; i++){
-                    speech = speech + " " + (i+1) + ". " + result.items[i].OrganizationName + ".\n";
-                    suggests.push({
-                        "title": result.items[i].OrganizationName
-                    })
+    Query( qString, loginEncoded, req, res, function(result) {
+        
+        if( result.items.length <= 0 ){
+            speech = "No records found";
+        }
+        else{
+            for(var i = 0; i < result.items.length; i++){
+                speech = speech + " " + (i+1) + ". " + result.items[i].OrganizationName + ".\n";
+                suggPatners.push({
+                    "title": result.items[i].OrganizationName
+                })
 
-                }  
-            }
+            }  
+        }
+        
+        
+        if( intentName == "KIR_Partners" ){
             SendResponse( speech, suggests, req, res, function(){
                 console.log("Finished!");
             });
+        }
+        else{
+            if( intentName == "KIR_Partners1" ){
+                
+            }
+        }
+        
             
-        });
+
+    });
+
+    
     
 }
