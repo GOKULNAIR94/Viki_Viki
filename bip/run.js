2e18a8c571ce7b98bc3a8c0ccbe43921567daa3e
Gokul Nair <gokulgnair94@gmail.com>
2018-02-24 19:48:03 +0530

    viki

--- bip/run.js
+++ bip/run.js
@@ -5,7 +5,7 @@ module.exports = function ( req, resp, callback){
     soap.createClient(url, function(err, client) {
         client.setSecurity(new soap.BasicAuthSecurity('LNT001', 'lntLNT2K16_1'));
         client.runReport(args, function(err, result) {
-            console.log(result);
+//            console.log(result);
             var obj = JSON.parse(result);
             console.log("Obj : " + obj.body);
         });
