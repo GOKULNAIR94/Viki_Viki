bdb845f830dbde2fab81f53536ed0019396774be
Gokul Nair <gokulgnair94@gmail.com>
2018-02-24 16:04:38 +0530

    BIP

--- bip/bip.js
+++ bip/bip.js
@@ -2,10 +2,8 @@ module.exports = function(req, res ) {
     var RUN = require("./run");
     
     var intentName = req.body.result.metadata.intentName;
-    
-    if ( intentName.indexOf("KIR_Partners") == 0  ) {
-        Run( req, res, function(result) {
-            console.log("Partners Called.");
-        });
-    }
+        
+    Run( req, res, function(result) {
+        console.log("Partners Called.");
+    });
 }
