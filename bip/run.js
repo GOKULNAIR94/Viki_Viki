97caa30ca585e2fd5c77e995a30ee5c35c680a3d
Gokul Nair <gokulgnair94@gmail.com>
2018-02-24 17:07:04 +0530

    BIP

--- bip/run.js
+++ bip/run.js
@@ -1,22 +1,31 @@
 module.exports = function ( req, resp, callback){ 
     var http = require("https");
-    console.log("RUN 1");
-    var soap = require('soap');
-    
-//    var methodName = "runReport";
-    try{
-        var soap = require('soap');
-        var url = 'https://acs.fs.ap2.oraclecloud.com/xmlpserver/services/v2/ReportService?wsdl';
-        var args = { Authorization: "Basic TE5UMDAxOmxudExOVDJLMTZfMQ=="};
-        soap.createClient(url, function(err, client) {
-            client.runReport(args, function(err, result) {
-                //console.log(result);
-            });
-        });
-    }
-    catch(e){
-        console.log("Error  :" + e);
-    }
-    
-    console.log("R 2");
+
+    var options = {
+      "method": "POST",
+      "hostname": "acs.fs.ap2.oraclecloud.com",
+      "port": null,
+      "path": "/xmlpserver/services/PublicReportService?wsdl=",
+      "headers": {
+        "content-type": "text/xml",
+        "cache-control": "no-cache",
+        "postman-token": "05fdc3e5-c2f2-753a-d53e-dcfdbed7fb7a"
+      }
+    };
+
+    var req = http.request(options, function (res) {
+      var chunks = [];
+
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+
+      res.on("end", function () {
+        var body = Buffer.concat(chunks);
+        console.log(body.toString());
+      });
+    });
+
+    req.write("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:pub=\"http://xmlns.oracle.com/oxp/service/PublicReportService\">\n   <soapenv:Header/>\n   <soapenv:Body>\n      <pub:runReport>\n         <pub:reportRequest>\n            <pub:reportAbsolutePath>Custom/BIPTest/Tickets.xdo</pub:reportAbsolutePath>\n         </pub:reportRequest>\n         <pub:userID>LNT001</pub:userID>\n         <pub:password>lntLNT2K16_1</pub:password>\n      </pub:runReport>\n   </soapenv:Body>\n</soapenv:Envelope>");
+    req.end();
 }
