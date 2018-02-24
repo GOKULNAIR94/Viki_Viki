ea7789e9f9699380c698e89f323465600b14badc
Gokul Nair <gokulgnair94@gmail.com>
2018-02-24 16:02:45 +0530

    BIP

--- bip/run.js
+++ bip/run.js
@@ -1,20 +1,27 @@
 module.exports = function ( req, resp, callback){ 
     var http = require("https");
-    console.LOG("RUN 1");
+    console.log("RUN 1");
     var soap = require('soap');
     var url = 'https://acs.fs.ap2.oraclecloud.com/xmlpserver/services/v2/ReportService?wsdl';
     var args = {};
-    soap.createClient(url, function( "runReport", args, headers, req) {
-        client.addSoapHeader(args, function(err, result) {
-            return {
-              Authorization: "Basic TE5UMDAxOmxudExOVDJLMTZfMQ=="
-            };
+    var methodName = "runReport";
+    try{
+        soap.createClient(url, function( methodName, args, headers, req) {
+            client.addSoapHeader(args, function(err, result) {
+                return {
+                  Authorization: "Basic TE5UMDAxOmxudExOVDJLMTZfMQ=="
+                };
 
+            });
+            client.methodName(args, function(err, result) {
+                console.log(result);
+                callback(result);
+            });
         });
-        client.runReport(args, function(err, result) {
-            console.log(result);
-            callback(result);
-        });
-    });
-    console.LOG("R 2");
+    }
+    catch(e){
+        console.log("Error  :" + e);
+    }
+    
+    console.log("R 2");
 }
