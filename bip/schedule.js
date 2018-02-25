9287e16decbc3c8f0bc78e48cdc03607e957224e
Gokul Nair <gokulgnair94@gmail.com>
2018-02-25 09:58:39 +0530

    viki

--- bip/schedule.js
+++ bip/schedule.js
@@ -114,14 +114,17 @@ module.exports = function ( req, resp, callback){
         "reportRequest":
         {
           "attributeFormat" :"HTML",     
-          "reportAbsolutePath" :"/Custom/TestDM_g.xdo"
-            }
+          "reportAbsolutePath" :"/Custom/TestDM_g.xdo",
+            "sizeOfDataChunkDownload" : -1
+            },
+        "userID" : "LNT001",
+        "password" : "lntLNT2K16_1"
         
     };
     
     soap.createClient(url, function(err, client) {
 
-        client.scheduleReport(args, "LNT001", "lntLNT2K16_1", function(err, result) {
+        client.scheduleReport(args, function(err, result) {
             console.log( "Schedule : " + JSON.stringify(result) );
             console.log( "Schedule : " + result.statusCode );
         });
