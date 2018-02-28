b89bd8865423be16c7d69e019e5d168a88bec143
Gokul Nair <gokulgnair94@gmail.com>
2018-03-01 01:53:22 +0530

    Leave Accurals

--- hcm/hcmleave_a.js
+++ hcm/hcmleave_a.js
@@ -12,17 +12,19 @@ module.exports = function(req, res, callback) {
     var suggests = [];
     var contextOut = [];
 
-    var empName = "";
-    empName = req.body.result.contexts[0].parameters['Name'];
-    console.log("empName : " + empName);
-    var firstName = toTitleCase(empName.split(" ")[0].toLowerCase());
-    var lastName = toTitleCase(empName.split(" ")[1].toLowerCase()) ;
     
-    console.log("Name : " + firstName + " " + lastName );
     
     
     switch (true) {
+            
             case (intentName == "hcm_leave_accruals"):{
+                var empName = "";
+                empName = req.body.result.contexts[0].parameters['Name'];
+                console.log("empName : " + empName);
+                var firstName = toTitleCase(empName.split(" ")[0].toLowerCase());
+                var lastName = toTitleCase(empName.split(" ")[1].toLowerCase()) ;
+                console.log("Name : " + firstName + " " + lastName );
+                
                 qString = "Select * from Employee WHERE FirstName='"+ firstName +"' AND LastName='" + lastName + "'";
                 break;
             }
