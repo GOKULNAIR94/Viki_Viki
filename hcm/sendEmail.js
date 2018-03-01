e6c64da06bf7d8e2f7dfc532ea4695d2eae4c1aa
Gokul Nair <gokulgnair94@gmail.com>
2018-03-01 23:37:07 +0530

    Leave Accurals

--- hcm/sendEmail.js
+++ hcm/sendEmail.js
@@ -49,23 +49,22 @@ module.exports = function( emailContent, req, res) {
             console.log(error);
         } else {
             console.log('Server is ready to take our messages');
-            transporter.sendMail(message, (error, info) => {
-                if (error) {
-                    console.log('Error occurred');
-                    console.log(error.message);
-                    return;
-                }
-                console.log('Message sent successfully!');
-                console.log('Server responded with "%s"', info.response);
-                transporter.close();
-            });
         }
     });
 
     
     console.log('Sending Mail');
     setTimeout(function() {
-        
+        transporter.sendMail(message, (error, info) => {
+            if (error) {
+                console.log('Error occurred');
+                console.log(error.message);
+                return;
+            }
+            console.log('Message sent successfully!');
+            console.log('Server responded with "%s"', info.response);
+            transporter.close();
+        });
         return res.json({
             speech: speech,
             displayText: speech,
