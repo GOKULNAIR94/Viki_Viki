399c415b0235c30007633c341bc3fea82a798d46
Gokul Nair <gokulgnair94@gmail.com>
2018-03-03 01:58:53 +0530

    accrual

--- hcm/hcmleave_a.js
+++ hcm/hcmleave_a.js
@@ -106,6 +106,9 @@ module.exports = function(req, res, callback) {
         QueryDB( qString, req, res, function(result) {
             if( result.rowsAffected == 0){
                 speech = "No records found.";
+                SendResponse(speech, suggests, contextOut, req, res, function() {
+                    console.log("Finished!");
+                });
             }else{
                 switch (true) {
                     case (intentName == "hcm_leave_accruals"):
