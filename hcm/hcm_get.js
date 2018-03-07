fb23a0504fd897e529268449700c9e886283c315
Gokul Nair <gokulgnair94@gmail.com>
2018-03-08 01:42:36 +0530

    new transfer

--- hcm/hcm_get.js
+++ hcm/hcm_get.js
@@ -6,6 +6,7 @@ module.exports = function(req, res, callback) {
     var Update = require("./update");
     var SendResponse = require("./sendResponse");
     var SendEmail = require("./sendEmail");
+    var HCMtransfer = require("./hcm_transfer");
     
     var speech = "";
     var suggests = [];
@@ -40,105 +41,100 @@ module.exports = function(req, res, callback) {
     }
     console.log("attrib : " + attrib + " " + attrib.length );
     
-    var qString = "/hcmCoreApi/resources/11.12.1.0/emps?q=FirstName=" + firstName + ";LastName=" + lastName + "&expand=assignments";
+    if( attribName != "Location"){
+        
+        
+        var qString = "/hcmCoreApi/resources/11.12.1.0/emps?q=FirstName=" + firstName + ";LastName=" + lastName + "&expand=assignments";
     
-    Query( qString, req, res, function(result) {
-        if (result.items.length == 0) {
-            speech = "No employee found by name " + empName + ".";
-            SendResponse(speech, suggests, contextOut, req, res, function() {
-                console.log("Finished!");
-            });
-        } else {
-            if (result.items.length >= 1) {
-                if(attrib.length == 2){
-                    if( intentName == "hcm_get_one_update"){
-                        console.log("Update : " + result.toString() );
-                        var empsId = result.items[0].links[0].href;
-                        console.log("href! : " + empsId );
-                        empsId = empsId.split("emps/")[1];
-                        console.log("empsId : " + empsId);
-                        qString = "/hcmCoreApi/resources/11.12.1.0/emps/" + empsId; //00020000000EACED00057708000110D9317FA60C0000004AACED00057372000D6A6176612E73716C2E4461746514FA46683F3566970200007872000E6A6176612E7574696C2E44617465686A81014B5974190300007870770800000161D9B5680078
-                        var attribValue = req.body.result.parameters['attribValue'];
-                        var body = {};
-                        body[attribCode] = attribValue;
-//                        body["attribName"] = attribName;
-                        
-                        Update( qString, body, success, req, res, function(result) {
-                            console.log("Update called");
-                            if( attribName == "Location"){
-                                var emailContent = {};
-                                emailContent.speech = firstName + " has been transferred to " + attribValue + ". \nAnything else I can help with?";
-                                emailContent.subject = "Your Transfer to " +attribValue+ " is processed";
-                                emailContent.body = '<p><b>Hello ' +firstName+',</b></p>' +
-                                    '<p>Your Transfer is processed. Please raise a request on IT Helpdesk for Domain Change.</p>' +
-                                    '<p>Thanks,<br><b>Viki</b></p>';
+        Query( qString, req, res, function(result) {
+            if (result.items.length == 0) {
+                speech = "No employee found by name " + empName + ".";
+                SendResponse(speech, suggests, contextOut, req, res, function() {
+                    console.log("Finished!");
+                });
+            } else {
+                if (result.items.length >= 1) {
+                    if(attrib.length == 2){
+                        if( intentName == "hcm_get_one_update"){
+                            console.log("Update : " + result.toString() );
+                            var empsId = result.items[0].links[0].href;
+                            console.log("href! : " + empsId );
+                            empsId = empsId.split("emps/")[1];
+                            console.log("empsId : " + empsId);
+                            qString = "/hcmCoreApi/resources/11.12.1.0/emps/" + empsId; //00020000000EACED00057708000110D9317FA60C0000004AACED00057372000D6A6176612E73716C2E4461746514FA46683F3566970200007872000E6A6176612E7574696C2E44617465686A81014B5974190300007870770800000161D9B5680078
+                            var attribValue = req.body.result.parameters['attribValue'];
+                            var body = {};
+                            body[attribCode] = attribValue;
+    //                        body["attribName"] = attribName;
 
-                                SendEmail( emailContent, req, res, function(result) {
-                                    console.log("SendEmail Called");
-                                });
-                                
-                            }else{
+                            Update( qString, body, success, req, res, function(result) {
+                                console.log("Update called");
                                 speech = firstName + "'s " + attribName + " has been updated to " + attribValue;
                                 speech = speech + " \nAnything else I can help you with?";
                                 SendResponse(speech, suggests, contextOut, req, res, function() {
                                     console.log("Finished!");
                                 });
-                            }
-                        });
-                    }
-                    else{
-                        speech =  firstName + "'s " + attribName + ": " + result.items[0][attribCode] + ".";
-                        speech = speech + " \n Anything else I can help you with?";
-        //                console.log("result : " + JSON.stringify(result.items[0]));
-                        SendResponse(speech, suggests, contextOut, req, res, function() {
-                            console.log("Finished!");
-                        });
-                    }
-                }else{
-                    if( attrib.length == 3){
-                        if( intentName == "hcm_get_one_update"){
-                             speech =  "Lets do that in the next version.";
+                            });
+                        }
+                        else{
+                            speech =  firstName + "'s " + attribName + ": " + result.items[0][attribCode] + ".";
+                            speech = speech + " \n Anything else I can help you with?";
             //                console.log("result : " + JSON.stringify(result.items[0]));
                             SendResponse(speech, suggests, contextOut, req, res, function() {
                                 console.log("Finished!");
                             });
                         }
-                        else{
-                            if(attribCode == "JobId"){
-                                var jobId = result.items[0].assignments[0].JobId;
-                                qString = "/hcmCoreSetupApi/resources/11.12.1.0/jobs?q=JobId=" + jobId + "&onlyData=true&&fields=Name";
-                            }
-
-                            if( attribCode == "ManagerId"){
-                                var manId = result.items[0].assignments[0].ManagerId;
-                                qString = "/hcmCoreApi/resources/11.12.1.0/emps?q=PersonId=" + manId + "&fields=DisplayName&onlyData=true";
-                            }
-
-                            Query( qString, req, res, function( collResult) {
-
-
-                                if(attribCode == "JobId"){
-                                    speech =  firstName + "'s " + attribName + ": " + collResult.items[0].Name + ".";
-                                }
-
-                                if( attribCode == "ManagerId"){
-                                    speech =  firstName + " reports to " + collResult.items[0].DisplayName + ".";
-                                }
-
-                                speech = speech + " \nAnything else I can help you with?";
+                    }else{
+                        if( attrib.length == 3){
+                            if( intentName == "hcm_get_one_update"){
+                                 speech =  "Lets do that in the next version.";
+                //                console.log("result : " + JSON.stringify(result.items[0]));
                                 SendResponse(speech, suggests, contextOut, req, res, function() {
                                     console.log("Finished!");
                                 });
+                            }
+                            else{
+                                if(attribCode == "JobId"){
+                                    var jobId = result.items[0].assignments[0].JobId;
+                                    qString = "/hcmCoreSetupApi/resources/11.12.1.0/jobs?q=JobId=" + jobId + "&onlyData=true&&fields=Name";
+                                }
 
-                            });
+                                if( attribCode == "ManagerId"){
+                                    var manId = result.items[0].assignments[0].ManagerId;
+                                    qString = "/hcmCoreApi/resources/11.12.1.0/emps?q=PersonId=" + manId + "&fields=DisplayName&onlyData=true";
+                                }
+
+                                Query( qString, req, res, function( collResult) {
+
+
+                                    if(attribCode == "JobId"){
+                                        speech =  firstName + "'s " + attribName + ": " + collResult.items[0].Name + ".";
+                                    }
+
+                                    if( attribCode == "ManagerId"){
+                                        speech =  firstName + " reports to " + collResult.items[0].DisplayName + ".";
+                                    }
+
+                                    speech = speech + " \nAnything else I can help you with?";
+                                    SendResponse(speech, suggests, contextOut, req, res, function() {
+                                        console.log("Finished!");
+                                    });
+
+                                });
+                            }
                         }
                     }
                 }
+
             }
-            
-        }
+
+
+        });
         
-        
-    });
+    }else{
+        HCMtransfer(req, res, function(result) {
+            console.log("HCM_transfer Called");
+        });
+    }
     
 }
--- hcm/hcm_transfer.js
+++ hcm/hcm_transfer.js
@@ -11,52 +11,79 @@ module.exports = function(req, res, callback) {
     var speech = "";
     var suggests = [];
     var contextOut = [];
+    var qString = "";
     
     var intentName = req.body.result.metadata.intentName;
+    var locValue = req.body.result.parameters['attribValue'];
+    var locationId ="";
 
     var empName = "";
+    
     empName = req.body.result.contexts[0].parameters['Name'];
     console.log("empName : " + empName);
     var firstName = toTitleCase(empName.split(" ")[0].toLowerCase());
     var lastName = toTitleCase(empName.split(" ")[1].toLowerCase()) ;
     console.log("Name : " + firstName + " " + lastName );
     
-    var qString = "/hcmCoreApi/resources/11.12.1.0/emps?q=FirstName=" + firstName + ";LastName=" + lastName;
+    qString = "/hcmCoreSetupApi/resources/11.12.1.0/locations?q=LocationName="+ locValue;
     
     Query( qString, req, res, function(result) {
         if (result.items.length == 0) {
-            speech = "No employee found by name " + empName + ".";
-            speech = speech +". \nIs there anything else I can help you with?";
-            SendResponse(speech, suggests, contextOut, req, res, function() {
-                console.log("Finished!");
-            });
-        } else {
-            console.log("Update : " + result.toString() );
-            var empsId = result.items[0].links[0].href;
-            console.log("href! : " + empsId );
-            empsId = empsId.split("emps/")[1];
-            console.log("empsId : " + empsId);
-            qString = "/hcmCoreApi/resources/11.12.1.0/emps/" + empsId; //00020000000EACED00057708000110D9317FA60C0000004AACED00057372000D6A6176612E73716C2E4461746514FA46683F3566970200007872000E6A6176612E7574696C2E44617465686A81014B5974190300007870770800000161D9B5680078
-            var attribValue = req.body.result.parameters['attribValue'];
-            var body = {};
-            body.City = attribValue;
-             
-            Update( qString, body, req, res, function() {
-                console.log("Update called");
-                var emailContent = {};
-                emailContent.speech = firstName + " has been transferred to " + attribValue + ". \nAnything else I can help you with?";
-                emailContent.subject = "Your Transfer to " +attribValue+ " is processed";
-                emailContent.body = '<p><b>Hello ' +firstName+',</b></p>' +
-                    '<p>Your Transfer is processed. Please raise a request on IT Helpdesk for Domain Change.</p>' +
-                    '<p>Thanks,<br><b>Viki</b></p>';
-                
-                SendEmail( emailContent, req, res, function(result) {
-                    console.log("SendEmail Called");
+                speech = "No location found by name " + locValue + ".";
+                speech = speech +". \nIs there anything else I can help you with?";
+                SendResponse(speech, suggests, contextOut, req, res, function() {
+                    console.log("Finished!");
                 });
-            });
-        }
+            } else {
+                locationId = result.items[0].LocationId;
+                qString = "/hcmCoreApi/resources/11.12.1.0/emps?q=FirstName=" + firstName + ";LastName=" + lastName;
+
+                Query( qString, req, res, function(result) {
+                    if (result.items.length == 0) {
+                        speech = "No employee found by name " + empName + ".";
+                        speech = speech +". \nIs there anything else I can help you with?";
+                        SendResponse(speech, suggests, contextOut, req, res, function() {
+                            console.log("Finished!");
+                        });
+                    } else {
+                        console.log("Update : " + result.toString() );
+                        var empsId = result.items[0].links[0].href;
+                        console.log("href! : " + empsId );
+                        empsId = empsId.split("emps/")[1];
+                        console.log("empsId : " + empsId);
+
+                        var assgId = result.items[0].assignments[0].links[0].href;
+                        console.log("href! assgId: " + assgId );
+                        assgId = assgId.split("/child/assignments/")[1];
+
+
+                        qString = "/hcmCoreApi/resources/11.12.1.0/emps/" + empsId + "/child/assignments/" + assgId; //00020000000EACED00057708000110D9317FA60C0000004AACED00057372000D6A6176612E73716C2E4461746514FA46683F3566970200007872000E6A6176612E7574696C2E44617465686A81014B5974190300007870770800000161D9B5680078
+
+                        var body = {};
+                        body.LocationId = locValue;
+
+                        Update( qString, body, req, res, function() {
+                            console.log("Update called");
+                            var emailContent = {};
+                            emailContent.speech = firstName + " has been transferred to " + locValue + ". \nAnything else I can help you with?";
+                            emailContent.subject = "Your Transfer to " +locValue+ " is processed";
+                            emailContent.body = '<p><b>Hello ' +firstName+',</b></p>' +
+                                '<p>Your Transfer is processed. Please raise a request on IT Helpdesk for Domain Change.</p>' +
+                                '<p>Thanks,<br><b>Viki</b></p>';
+
+                            SendEmail( emailContent, req, res, function(result) {
+                                console.log("SendEmail Called");
+                            });
+                        });
+                    }
+
+
+                });
+            }
         
         
     });
+    
+    
     
 }
