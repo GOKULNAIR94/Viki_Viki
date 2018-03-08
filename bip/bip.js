d52c2503e68885203c24f5054fcccbe3ceae915c
Gokul Nair <gokulgnair94@gmail.com>
2018-03-09 00:19:24 +0530

    fb rich

--- bip/bip.js
+++ bip/bip.js
@@ -17,7 +17,7 @@ module.exports = function(req, res ) {
     
     Schedule( req, res, function(result) {
         console.log("Schedule Called.");
-        speech = "You request has been scheduled. Job Id : " + result.scheduleReportReturn + ". The payslip will be generated and mailed to you in a couple of minutes. Anything else I help you with?";
+        speech = "You request has been scheduled. Job Id : " + (result.scheduleReportReturn +1 ) + ". The payslip will be generated and mailed to you in a couple of minutes. Anything else I can help you with?";
         SendResponse(speech, suggests, contextOut, req, res, function() {
             console.log("Finished!");
         });
