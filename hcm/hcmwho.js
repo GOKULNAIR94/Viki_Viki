4131dd17219ec72e10f13a0b88ab7bbe0e2a43f2
Gokul Nair <gokulgnair94@gmail.com>
2018-03-08 11:05:19 +0530

    new transfer

--- hcm/hcmwho.js
+++ hcm/hcmwho.js
@@ -38,8 +38,10 @@ module.exports = function(req, res, callback) {
                 
                 var manId = result.items[0].assignments[0].ManagerId;
                 var jobId = result.items[0].assignments[0].JobId;
+                var locIdlocId = result.items[0].assignments[0].LocationId;
                 console.log("ManagerId : " + manId);
                 console.log("job : " + jobId);
+                console.log("locId : " + locId);
                 
                 
 
@@ -47,40 +49,49 @@ module.exports = function(req, res, callback) {
                     speech = speech + ",\nEmail: " + result.items[0].WorkEmail;
                 if( result.items[0].WorkPhoneNumber != null && result.items[0].WorkPhoneNumber != "" )
                     speech = speech + ",\nPhone: " + result.items[0].WorkPhoneNumber;
-                if( result.items[0].City != null && result.items[0].City != "" )
-                    speech = speech + ",\nLocation: " + result.items[0].City;
-//                        speech = speech + ",\n" + result.items[0].AddressLine1 +  ", " + result.items[0].City +  ", " + result.items[0].Country;
                 
-                if( manId != null && manId != "" && jobId != null && jobId != ""){
-                    qString = "/hcmCoreApi/resources/11.12.1.0/emps?q=PersonId=" + manId + "&fields=DisplayName&onlyData=true";
-                    Query( qString, req, res, function( manResult) {
-                        var manName = manResult.items[0].DisplayName;
-                        qString = "/hcmCoreSetupApi/resources/11.12.1.0/jobs?q=JobId=" + jobId + "&onlyData=true&&fields=Name";
+//                        speech = speech + ",\n" + result.items[0].AddressLine1 +  ", " + result.items[0].City +  ", " + result.items[0].Country;
+                if(locId != null && locId != ""){
+                    qString = "/hcmCoreApi/resources/11.12.1.0/locations?q=LocationId=" + locId + "&fields=LocationName&onlyData=true";
+                    Query( qString, req, res, function( locResult) {
+                        var locName = locResult.items[0].LocationName;
+                        if( locName != null && locName != "" )
+                                speech = speech + ",\Location: " + locName;
+                        
+                        if( manId != null && manId != "" && jobId != null && jobId != ""){
+                            qString = "/hcmCoreApi/resources/11.12.1.0/emps?q=PersonId=" + manId + "&fields=DisplayName&onlyData=true";
+                            Query( qString, req, res, function( manResult) {
+                                var manName = manResult.items[0].DisplayName;
+                                qString = "/hcmCoreSetupApi/resources/11.12.1.0/jobs?q=JobId=" + jobId + "&onlyData=true&&fields=Name";
 
-                        Query( qString, req, res, function( jobResult) {
-                            var jobName = jobResult.items[0].Name;
-                            if( jobName != null && jobName != "" )
-                                speech = speech + ",\nDesignation: " + jobName;
+                                Query( qString, req, res, function( jobResult) {
+                                    var jobName = jobResult.items[0].Name;
+                                    if( jobName != null && jobName != "" )
+                                        speech = speech + ",\nDesignation: " + jobName;
 
-                            if( manName != null && manName != "" )
-                                speech = speech + ",\nReporting Manager: " + manName;
+                                    if( manName != null && manName != "" )
+                                        speech = speech + ",\nReporting Manager: " + manName;
 
-                           
+
+                                    speech = speech +". \nAnything else I can help you with?";
+                                    SendResponse(speech, suggests, contextOut, req, res, function() {
+                                        console.log("Finished!");
+                                    });
+
+
+                                });
+                            });
+                        }
+                        else{
                             speech = speech +". \nAnything else I can help you with?";
                             SendResponse(speech, suggests, contextOut, req, res, function() {
                                 console.log("Finished!");
                             });
-
-
-                        });
+                        }
                     });
                 }
-                else{
-                    speech = speech +". \nAnything else I can help you with?";
-                    SendResponse(speech, suggests, contextOut, req, res, function() {
-                        console.log("Finished!");
-                    });
-                }
+                
+                
             }
             
             
